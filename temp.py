3ca580d0a8606499ab763838ef5ed2a38a6fe0ec
zeiron <56308652+zeiron-crypto@users.noreply.github.com>
2019-11-19 17:46:43 +0200

    Add files via upload

--- temp.py
+++ temp.py
@@ -2,31 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import ArtistAnimation
 fig = plt.figure()
-def parabol(a, b, c, n):
-    x = np.linspace(-5, 5, n)
-    y = np.zeros(n)
-    for i in range(n):
-        y[i] = a * x[i] ** 2 + b * x[i] + c
-    return x, y
-def cir(x_centre, y_centre, r, n):
+def circle_func(x_centre_point, y_centre_point, r, n):
     x = np.zeros(n)
     y = np.zeros(n)
-    alpha = np.linspace(0, 2 * np.pi, n)
-    for i in range(n):
-        x[i] = x_centre + r * np.cos(alpha[i])
-        y[i] = y_centre + r * np.sin(alpha[i])
+    for i in range(0 ,n, 1):
+        alpha = np.linspace(0, 2 * np.pi, n)
+        x[i] = x_centre_point + r * np.cos(alpha[i])
+        y[i] = y_centre_point + r * np.sin(alpha[i])
     return x, y
+thetas = np.linspace(0, 4 * np.pi, 100)
+r = 16
 n = 100
+x_centre = r * np.cos(thetas) ** 3
+y_centre = r * np.sin(thetas) ** 3
 anim_list = []
-x = []
-y = []
-for i in range(n):
-    x, y = parabol(1, 1, 0, n)
-    x_cir, y_cir = cir(x[i], y[i], 3, n)
-    circle, = plt.plot(x_cir, y_cir, 'g-', lw=2)
-    parabola, = plt.plot(x[:i+1], y[:i+1], 'r-', lw=2)
-    point, = plt.plot(x[i], y[i], 'o', markersize=4)
-    anim_list.append([parabola, circle, point])
+for i in range(0 ,n ,1):
+    x, y = circle_func(x_centre[i], y_centre[i], r/4, n)
+    circle, = plt.plot(x, y, 'g-', lw=2)
+    circle_main, = plt.plot(x, y, 'r-', lw=2)
+    point, = plt.plot(x_centre[i], y_centre[i], 'bo', markersize=4)
+    anim_list.append([circle, point])
 ani = ArtistAnimation(fig, anim_list, interval=50)
-plt.axes().set_aspect('equal')
-ani.save('anim.gif')
+plt.axis('equal')
+ani.save('anim1.gif')
