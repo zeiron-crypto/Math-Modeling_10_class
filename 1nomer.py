0c79c0b26a51752cd26aa37c7456fcab9e355e82
zeiron <56308652+zeiron-crypto@users.noreply.github.com>
2020-01-28 17:22:26 +0200

    Update 1nomer.py

--- 1nomer.py
+++ 1nomer.py
@@ -13,3 +13,22 @@ g=10
 T=200
 hl=6,6*10**-34
 
+fig = plt.figure()
+bodys = []
+
+for i in range (0, len(t), 1):
+    body1, = plt.plot(so[:i, 0], sol[:i, 2], '-', color ='r')
+    body1_line, = plt.plot(sol[i, 0], sol[i, 2], 'o', color = 'r')
+    
+    body2, = plt.plot(so[:i,4], sol[:i,6], '-', color ='g')
+    body2_line, = plt.plot(sol[i,4], sol[i, 6], 'o', color = 'g')
+     
+    body3, = plt.plot(so[:i,8], sol[:i,10], '-', color ='b') 10
+    body3_line, = plt.plot(sol[i,8], sol[i,10], 'o', color = 'b')
+    
+    bodys.append([body1, body1_line, body2, body2_line, body3, body3_line])
+    
+ani = ArtistAnimation(fig, bodys, interval=50)
+plt.axis('equal')
+ani.save('N_body_G+K.gif')
+    
